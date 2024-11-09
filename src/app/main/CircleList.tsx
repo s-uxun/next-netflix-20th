7682e705502bf7a41536e32ae131484a70b7396e
s-uxun <serenasol@ewha.ac.kr>
2024-11-09 13:42:37 +0900

    Design: complete CircleList

--- src/app/main/CircleList.tsx
+++ src/app/main/CircleList.tsx
@@ -2,15 +2,15 @@ import { Movie } from '@api/types';
 
 const CircleList = ({ movies, title }: { movies: Movie[]; title: string }) => {
   return (
-    <div className="pl-4">
-      <p className="text-2xl font-bold">{title}</p>
-      <div className="flex overflow-scroll">
+    <div className="pl-4 mb-4">
+      <p className="text-3xl font-bold mb-6">{title}</p>
+      <div className="flex overflow-scroll gap-2">
         {movies.map((movie) => (
           <img
             key={movie.id}
             src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
             alt={movie.title}
-            className=""
+            className="h-24 min-w-24 object-cover rounded-full"
           />
         ))}
       </div>

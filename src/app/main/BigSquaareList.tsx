7a0b044a44ff7d74f5b6a9fe24c2e8ed6f09b3a0
s-uxun <serenasol@ewha.ac.kr>
2024-11-09 16:48:10 +0900

    Feat: Connect detail page with Square & BigSquare components

--- src/app/main/BigSquaareList.tsx
+++ src/app/main/BigSquaareList.tsx
@@ -1,4 +1,5 @@
 import { Content } from '@api/types';
+import Link from 'next/link';
 
 const BigSquareList = ({
   contents,
@@ -12,12 +13,17 @@ const BigSquareList = ({
       <p className="text-xl font-bold mb-4">{title}</p>
       <div className="flex overflow-scroll gap-2">
         {contents.map((content) => (
-          <img
+          <Link
             key={content.id}
-            src={`https://image.tmdb.org/t/p/original${content.poster_path}`}
-            alt={content.title}
-            className="h-60 min-w-36 object-cover rounded-sm cursor-pointer hover:scale-105 hover: my-1"
-          />
+            href={`/details/${content.media_type}/${content.id}`}
+          >
+            <img
+              key={content.id}
+              src={`https://image.tmdb.org/t/p/original${content.poster_path}`}
+              alt={content.title}
+              className="h-60 min-w-36 object-cover rounded-sm cursor-pointer hover:scale-105 hover: my-1"
+            />
+          </Link>
         ))}
       </div>
     </div>
--- src/app/main/SquareList.tsx
+++ src/app/main/SquareList.tsx
@@ -1,4 +1,5 @@
 import { Content } from '@api/types';
+import Link from 'next/link';
 
 const SquareList = ({
   contents,
@@ -12,12 +13,17 @@ const SquareList = ({
       <p className="text-xl font-bold mb-3">{title}</p>
       <div className="flex content-center overflow-scroll gap-2">
         {contents.map((content) => (
-          <img
+          <Link
             key={content.id}
-            src={`https://image.tmdb.org/t/p/original${content.poster_path}`}
-            alt={content.title}
-            className="h-40 min-w-24 object-cover rounded-sm cursor-pointer hover:scale-105 my-1"
-          />
+            href={`/details/${content.media_type}/${content.id}`}
+          >
+            <img
+              key={content.id}
+              src={`https://image.tmdb.org/t/p/original${content.poster_path}`}
+              alt={content.title}
+              className="h-40 min-w-24 object-cover rounded-sm cursor-pointer hover:scale-105 my-1"
+            />
+          </Link>
         ))}
       </div>
     </div>

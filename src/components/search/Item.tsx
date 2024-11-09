c7c1a8b862273532699ec83708cef2a5f4c9ea8c
JiwonChoi0805 <won0805@ewhain.net>
2024-11-09 18:32:13 +0900

    feat: route from search to detail

--- src/components/search/Item.tsx
+++ src/components/search/Item.tsx
@@ -1,23 +1,27 @@
+import Link from 'next/link';
 import PlayBtn from '@public/icons/play_btn.svg';
 
 interface ItemProps {
+  id: number;
   title: string;
   posterUrl: string;
 }
 
-export default function Item({ title, posterUrl }: ItemProps) {
+export default function Item({ id, title, posterUrl }: ItemProps) {
   return (
-    <span className="flex flex-row w-full h-[4.75rem] bg-[#424242] mb-[0.19rem]">
-      <span className="w-[9.125rem] h-full">
-        <img
-          src={`https://image.tmdb.org/t/p/w500${posterUrl}`}
-          className="w-full h-full object-cover"
-        />
+    <Link href={`/details/movie/${id}`}>
+      <span className="flex flex-row w-full h-[4.75rem] bg-[#424242] mb-[0.19rem] hover:translate-x-1">
+        <span className="w-[9.125rem] h-full">
+          <img
+            src={`https://image.tmdb.org/t/p/w500${posterUrl}`}
+            className="w-full h-full object-cover"
+          />
+        </span>
+        <span className="flex flex-row w-[14.31rem] items-center pl-[1.13rem] pr-3 justify-between">
+          <span className="text-White w-40 text-[0.92013rem]">{title}</span>
+          <PlayBtn />
+        </span>
       </span>
-      <span className="flex flex-row w-[14.31rem] items-center pl-[1.13rem] pr-3 justify-between">
-        <span className="text-White w-40 text-[0.92013rem]">{title}</span>
-        <PlayBtn />
-      </span>
-    </span>
+    </Link>
   );
 }

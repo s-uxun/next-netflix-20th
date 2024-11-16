ab60fb5d1591afe53154efa982b9e0da6fd31c20
s-uxun <serenasol@ewha.ac.kr>
2024-11-16 18:58:12 +0900

    Feat: axios -> fetch (use revalidate)

--- src/api/getData.ts
+++ src/api/getData.ts
@@ -1,18 +1,17 @@
-import instance from './instance';
 import { Content } from './types';
 
 export const getContents = async (category: string) => {
   try {
-    const res = await instance.get(`${category}`, {
-      params: {
-        language: 'en-US',
-        page: 1,
-      },
+    const url = `${process.env.NEXT_PUBLIC_URL}${category}?language=en-US&page=1&api_key=${process.env.NEXT_PUBLIC_API_KEY}`;
+
+    const res = await fetch(url, {
+      next: { revalidate: 300 }, // 5분 동안 데이터 캐싱하도록 함(매번 새로 불러오지 않도록)
     });
 
+    const data = await res.json();
     const media_type = category.includes('/tv') ? 'tv' : 'movie';
 
-    return res.data.results.map((item: Content) => ({
+    return data.results.map((item: Content) => ({
       ...item,
       media_type,
     }));
@@ -32,23 +31,27 @@ export const getAllMovies = async () => {
     '/tv/popular',
     '/tv/top_rated',
   ];
+
   try {
     const movies = await Promise.all(categories.map(getContents));
     return movies;
   } catch (error) {
+    console.error('Error fetching movies:', error);
     return [];
   }
 };
 
 export const getDetails = async (media_type: string, id: string) => {
   try {
-    const res = await instance.get(`/${media_type}/${id}`, {
-      params: {
-        language: 'en-US',
-      },
+    const url = `${process.env.NEXT_PUBLIC_URL}/${media_type}/${id}?language=en-US&api_key=${process.env.NEXT_PUBLIC_API_KEY}`;
+
+    const res = await fetch(url, {
+      next: { revalidate: 300 },
     });
-    return res.data;
+
+    const data = await res.json();
+    return data;
   } catch (error) {
-    console.log(error);
+    return null;
   }
 };

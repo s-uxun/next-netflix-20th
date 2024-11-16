6d2e6db3a0848ea9d63ab3137be2be9fb8558945
s-uxun <serenasol@ewha.ac.kr>
2024-11-16 20:23:21 +0900

    Feat: Update next.config.ts with images.remotePatterns

--- src/components/main/BigSquareList.tsx
+++ src/components/main/BigSquareList.tsx
@@ -1,5 +1,6 @@
 import { Content } from '@api/types';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const BigSquareList = ({
   contents,
@@ -17,12 +18,17 @@ const BigSquareList = ({
             key={content.id}
             href={`/details/${content.media_type}/${content.id}`}
           >
-            <img
-              key={content.id}
-              src={`https://image.tmdb.org/t/p/original${content.poster_path}`}
-              alt={content.title}
-              className="h-60 min-w-36 object-cover rounded-sm cursor-pointer hover:scale-105 hover: my-1"
-            />
+            <div className="h-60 min-w-36 relative">
+              {' '}
+              {/* Image를 사용할 때 부모 div에 상대적 크기를 설정 */}
+              <Image
+                src={`https://image.tmdb.org/t/p/original${content.poster_path}`}
+                alt={content.title || content.name || ''}
+                layout="fill" // 이미지가 부모 div의 크기에 맞게 채워지도록 설정
+                objectFit="cover" // 기존 object-cover 스타일과 동일하게 설정
+                className="rounded-sm cursor-pointer hover:scale-105 my-1"
+              />
+            </div>
           </Link>
         ))}
       </div>
